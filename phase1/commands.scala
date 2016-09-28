e93c5c019075d8e1ecee36461a25d9003e749902
vrjdev <vrj.dev@gmail.com>
2016-09-28 14:39:23 -0700

    Added phase1 folder. Added commands.scala file to write scala commands

---
+++ phase1/commands.scala
@@ -0,0 +1,8 @@
+import org.datasyslab.geospark.spatialOperator.JoinQuery;
+import org.datasyslab.geospark.spatialOperator.KNNQuery;
+import org.datasyslab.geospark.spatialOperator.RangeQuery;
+import org.datasyslab.geospark.spatialRDD.PointRDD;
+import org.datasyslab.geospark.spatialRDD.PolygonRDD;
+import org.datasyslab.geospark.spatialRDD.RectangleRDD;
+
+
