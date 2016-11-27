66b818fac814f41f638ca7df5e51dba3bdaf24b6
slang06 <slang@att.net>
2016-11-26 18:37:02 -0700

    phase3 initial commit.

---
+++ phase3/phase3.java
@@ -0,0 +1,29 @@
+import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.api.java.JavaRDD
+import org.apache.spark.SparkConf
+
+public class Phase2 implements Serializable{
+
+		
+    public static float meanValue() 
+    {
+
+    
+
+    }
+
+    public static float standardDev() 
+    {
+
+    
+
+    }
+
+    public static float getisValue() 
+    {
+
+    
+
+    }
+
+}
