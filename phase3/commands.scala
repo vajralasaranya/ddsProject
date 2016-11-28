e396f6673e4729b44fd639ced8041780e1d41e9a
slang06 <slang@att.net>
2016-11-27 18:50:31 -0700

    made progress on this phase.

---
+++ phase3/commands.scala
@@ -0,0 +1,64 @@
+import org.datasyslab.geospark.spatialOperator.JoinQuery;
+import org.datasyslab.geospark.spatialOperator.KNNQuery;
+import org.datasyslab.geospark.spatialOperator.RangeQuery;
+import org.datasyslab.geospark.spatialRDD.PointRDD;
+import org.datasyslab.geospark.spatialRDD.PolygonRDD;
+import org.datasyslab.geospark.spatialRDD.RectangleRDD;
+
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.broadcast.Broadcast;
+import org.apache.spark.storage.StorageLevel;
+import org.datasyslab.geospark.spatialOperator.JoinQuery;
+import org.datasyslab.geospark.spatialOperator.KNNQuery;
+import org.datasyslab.geospark.spatialOperator.RangeQuery;
+import org.datasyslab.geospark.spatialRDD.PointRDD;
+import org.datasyslab.geospark.spatialRDD.PolygonRDD;
+import org.datasyslab.geospark.spatialRDD.RectangleRDD;
+
+import com.vividsolutions.jts.geom.GeometryFactory;
+import org.datasyslab.geospark.spatialOperator.RangeQuery; 
+import org.datasyslab.geospark.spatialRDD.PointRDD;
+import com.vividsolutions.jts.geom.Envelope;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+import java.io.PrintWriter;
+
+
+
+// set the envelope size = lat and long of all NYC districts 
+val queryEnvelope=new Envelope (74.25,73.7,40.5,40.9);
+
+// load the data file 
+val objectRDD = new PointRDD(sc, "hdfs://rajesh:54310/home/dataset/arealm.csv", 0, "csv"); 
+
+// NEED TO DO: Parse only the date, pickup_lat , pickup_long <----------------------------------------
+
+//gets the points within the bounds of the 5 districts 
+val points_in_bounds = RangeQuery.SpatialRangeQuery(objectRDD, queryEnvelope, 0).getRawPointRDD();
+
+//group the data based on days   
+
+
+//now for each block in the envelope find how many trips occured in one blk
+
+
+// now calculate the G and Mean scores for each 
--- phase3/phase3.java
+++ phase3/phase3.java
@@ -2,27 +2,72 @@ import org.apache.spark.api.java.JavaSparkContext
 import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.SparkConf
 
-public class Phase2 implements Serializable{
+public class Phase3 implements Serializable{
 
-		
-    public static float meanValue() 
+
+    public static float standardDev(float mean , int total_cells , float sum_x_squared) 
     {
+    	float standDev = Math.sqrt((sum_x_squared / total_cells ) - Math.pow(mean,2));
+    	return standDev;
+    }
 
-    
+    public static float getis_OrdValue()
+    {
 
     }
 
-    public static float standardDev() 
+
+    //this function will be used on one day of data - need to add the data to this function delceration 
+    public static void groupCellData(PointRDD objectRDD)
     {
+    	//constants for cell boundires 
+    	float latitude_min = 40.5;
+    	float latitude_max = 40.9;
+    	float longitude_min = 73.7;
+    	float longitude_max = 74.25;
+    	float cell_length = .01;
 
-    
+    	float lat_boundry = 0.0;
+    	float long_boundry = 0.0;
 
-    }
+    	int total_cells = 0; 
 
-    public static float getisValue() 
-    {
+    	//mean Value 
+    	float mean_Value = 0.0;
 
-    
+    	//standard dev values 
+    	float x_squared_sum = 0.0;
+    	float standard_Dev_value =0.0;
+
+    	//for each latitude itterate over all cells
+    	for(int i=latitude_min; i <= (latitude_max - cell_length); i=i+cell_length)
+    	{
+    		//itterate all longitude blocks 
+    		for(int j=longitude_min; j <= (longitude_max - cell_length); j=j+cell_length)
+    		{
+    			lat_boundry = i + cell_length;
+    			long_boundry = j + cell_length;
+    			//collect all values within the cell blk
+    			//set boundry envelope 
+    			val queryEnvelope=new Envelope (long_boundry,j,i,lat_boundry);
+    			//query for the values within the envelope 
+    			val points_in_cell = RangeQuery.SpatialRangeQuery(objectRDD, queryEnvelope, 0).getRawPointRDD().count();
+    			mean_Value += points_in_cell;
+    			//square and sum the value for this cell - used for standard dev 
+    			x_squared_sum += Math.pow(points_in_cell,2);
+
+    			total_cells++;
+
+    		} // end of inner for loop
+
+    		total_cells++;
+    	}//end of for loop
+
+    	//calcualte the mean value for all the cells 
+    	mean_Value = mean_Value / total_cells;
+
+    	//calc standard dev 
+    	standard_Dev_value = standardDev(mean_Value,total_cells,x_squared_sum);
 
     }
 
